0a9c0607411bd174c9d8f30b335e42aa9d4ba14d
ayangweb <75017711+ayangweb@users.noreply.github.com>
2025-03-28 23:36:24 +0800

    chore: remove '@unocss/preset-rem-to-px' dependency (#8)

--- uno.config.ts
+++ uno.config.ts
@@ -1,4 +1,3 @@
-import presetRemToPx from '@unocss/preset-rem-to-px'
 import {
   defineConfig,
   presetIcons,
@@ -11,9 +10,6 @@ export default defineConfig({
   presets: [
     presetWind3(),
     presetIcons(),
-    presetRemToPx({
-      baseFontSize: 4,
-    }),
   ],
   transformers: [
     transformerVariantGroup(),
