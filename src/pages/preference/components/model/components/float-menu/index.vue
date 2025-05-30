5152ca078504ec16cb79c4a423b69990e6ff6cc6
ayangweb <75017711+ayangweb@users.noreply.github.com>
2025-05-30 14:52:58 +0800

    feat: 模型管理页面新增悬浮操作菜单 (#410)

---
+++ src/pages/preference/components/model/components/float-menu/index.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { EditOutlined, MenuOutlined, UnorderedListOutlined } from '@ant-design/icons-vue'
+import { openUrl } from '@tauri-apps/plugin-opener'
+import { FloatButton, FloatButtonGroup } from 'ant-design-vue'
+</script>
+
+<template>
+  <FloatButtonGroup
+    class="bottom-4 right-4"
+    trigger="click"
+    type="primary"
+  >
+    <template #icon>
+      <MenuOutlined />
+    </template>
+
+    <FloatButton
+      tooltip="如何制作模型？"
+      @click="openUrl('https://juejin.cn/post/7509872655802269731')"
+    >
+      <template #icon>
+        <EditOutlined />
+      </template>
+    </FloatButton>
+
+    <FloatButton
+      tooltip="更多模型"
+      @click="openUrl('https://github.com/ayangweb/Awesome-BongoCat')"
+    >
+      <template #icon>
+        <UnorderedListOutlined />
+      </template>
+    </FloatButton>
+  </FloatButtonGroup>
+</template>
